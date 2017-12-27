8c8fe2bc5685af7bb2bb604b296b8edbb48ccd44
KoalaMuffin <jcu123123@gmail.com>
2017-12-27 16:35:40 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-read "jcForum requires MySQL database access. What is the MySQL username (i.e, root)" -r $dbUser
-read "What is the MySQL password?" -r $dbPassword
+read -p "jcForum requires MySQL database access. What is the MySQL username (i.e, root)" dbUser
+read -p "What is the MySQL password?" dbPassword
 
 $(mysql -u$dbUser -p$dbPassword -se "CREATE DATABASE jcForum")
 
-sudo mkdir /jcForum
-sudo echo "\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcForum\");" > /jcForum/mysql.php
+sudo mkdir /lib/jcForum
+sudo echo "\$mysqli = new mysqli(\"127.0.0.1\", \"$dbUser\", \"$dbPassword\", \"jcForum\");" | sudo tee /lib/jcForum/mysql.php
+
+$(mysql jcForum -u$dbUser -p$dbPassword -se "CREATE TABLE forumInfo (path varchar(128));")
+read -p "What path should your forum be at (i.e, /path/to/server/forum)" path
+$(mysql jcForum -u$dbUser -p$dbPassword -se "INSERT INTO forumInfo (path) VALUES ('$path')")
